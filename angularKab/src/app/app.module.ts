1d52125ab1d6f970e15522f3b4a03b281ec4e149
philipsenwout <philipsenwout@outlook.com>
2018-02-22 14:02:35 +0100

    begin minesweeper

--- angularKab/src/app/app.module.ts
+++ angularKab/src/app/app.module.ts
@@ -12,7 +12,7 @@ import { LoginComponent } from './login/login.component';
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
